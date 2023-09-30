dec7aaf60a068b61d7971a41909bf57ad02982b5
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-09-30 11:07:43 +0100

    Add unit test for registration with empty phone number

--- src/test/java/com/example/springbootfullproject/controller/UserControllerTest.java
+++ src/test/java/com/example/springbootfullproject/controller/UserControllerTest.java
@@ -234,4 +234,39 @@ class UserControllerTest {
                     }
                 });
     }
+
+    @Test
+    public void givenPhoneNumberIsEmpty_whenRegisterUser_thenThrowMethodArgumentNotValidException() throws Exception {
+
+        RegistrationRequest request = RegistrationRequest.builder()
+                .firstName("test")
+                .lastName("test")
+                .email("test@soufianezouhair.com")
+                .phoneNumber("")
+                .password("Pass@123")
+                .role("TEACHER")
+                .build();
+
+        mockMvc.perform(
+                        post("/api/v1/user/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(JsonUtils.serializeToJson(request))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(result -> {
+                    assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException);
+
+                    MethodArgumentNotValidException ex = (MethodArgumentNotValidException) result.getResolvedException();
+                    List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+                    assertThat(fieldErrors, hasSize(greaterThanOrEqualTo(1))
+                    );
+                    // Assert the specific field error(s) and their messages
+                    for (FieldError fieldError : fieldErrors) {
+                        if ("phoneNumber".equals(fieldError.getField())) {
+                            assertEquals("Phone Number is required.", fieldError.getDefaultMessage());
+                        }
+                    }
+                });
+    }
 }
