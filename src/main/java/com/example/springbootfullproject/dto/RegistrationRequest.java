50f8523420c2b36644b63e07121028128962dc87
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-09-30 11:17:24 +0100

    Add not empty and not null validation for password

--- src/main/java/com/example/springbootfullproject/dto/RegistrationRequest.java
+++ src/main/java/com/example/springbootfullproject/dto/RegistrationRequest.java
@@ -17,6 +17,7 @@ public class RegistrationRequest {
     private final String email;
     @NotEmpty(message = "Phone number is required.")
     private final String phoneNumber;
+    @NotEmpty(message = "Password is required.")
     private final String password;
     private final String role;
 
